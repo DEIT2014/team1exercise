776e988b137eb8719c8b5ba50220e7e38f765906
shirleysr <shirui1203@qq.com>
2016-09-29 22:04:41 +0800

    COMPLETE

--- web/main.dart
+++ web/main.dart
@@ -1,30 +1,26 @@
-// Copyright (c) 2016, wqwtsr. All rights reserved. Use of this source code
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
 
 void main(){
-  querySelector('#sample_text_id')
-    .. text = "2,5,1,8,7"
-    ..onClick.listen(sort);
+  querySelector('#output').text = 'running';
+  querySelector('#showme').text = '2,5,1,8,7';
+  querySelector('#sample_text_id').onClick.listen(sort);
 }
-
-  void sort(MouseEvent event){
-    var text = querySelector('#sample_text_id').text;
-    var buffer = new StringBuffer();
-    for (int i = text.length - 1; i >= 0; i--) {
-      buffer.write(text[i]);
-    }
-    int i,j;
-    for(i=2;i<=text.length;++i)
-      if (int.parse(text[i])<int.parse(text[i-1]))
-      {
-      int.parse(text[0])=int.parse(text[i]);
-      int.parse(text[i])=int.parse(text[i-1]);
-        for(j=i-2;int.parse(text[0])<int.parse(text[j]);--j)
-      int.parse(text[j+1])=int.parse(text[0]);
-      }
-    for ( i = 0; i <= text.length; i++) {
-      buffer.write(text[i]);}
-    querySelector('#sample_text_id').text = buffer.toString();
-  }
+void sort(MouseEvent event) {
+  var text = querySelector('#sample_text_id').text;
+  var list=new List();
+  list=[2,5,1,8,7];
+  int i, j,k;
+  for (i = 0; i< list.length;i++) {
+    for(j = 0; j< list.length-i-1;j++){
+      if (list[j]> list[j+1])
+    {k= list[j];
+    list[j] = list[j+1];
+    list[j+1] =k; }
+  }}
+  var buffer = new StringBuffer();
+  for (int i =0;i< list.length;i++) {
+    buffer.write(list[i]);querySelector('#showme').text = buffer.toString();
+}}
